cd68339c12534f5fca605a88d509f8ecc3364d04
Konstantin Dunaev <shop.overled@gmail.com>
2021-03-22 23:27:57 +0300

    add code

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int ticket_cost_rub = 2137;
+        int miles_step_rub = 20;
+        int miles = ticket_cost_rub/miles_step_rub;
+        System.out.println(miles);
+    }
+}
